2071ccd28bb54faec01c78564e112483bd3700da
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-06 23:47:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end 
   
   get "/multiply/:num1/:num2" do 
-    @num1 = params[:id]
-    @num2 = params[:id]
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i 
     "#{@num1 * @num2}"
   end 
 
